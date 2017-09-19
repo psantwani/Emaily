599c4cd4d8bf13a3aa22e3bf4742ebcf1045d7ec
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-09-19 18:13:38 +0530

    mongoose setup complete

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,9 +10,17 @@ module.exports = app => {
 
   app.get(
     '/auth/google/callback',
-    passport.authenticate('google'),
-    (req, res) => {
-      res.redirect('/surveys');
-    }
+    passport.authenticate('google')
   );
+
+  app.get('/api/current_user', (req, res) => {
+    //res.send(req.session);
+    res.send(req.user);
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
 }
