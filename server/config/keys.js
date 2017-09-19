2bca66695d60036d7e35133819657d81a9e4e86a
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-09-19 13:42:22 +0530

    setting passport up

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '769136930839-5efqso4kf3pfc2vsscmhvmfpk40hq347.apps.googleusercontent.com',
+    googleClientSecret: 'SiTQUHynuUYH5lDnTd7irsx4',
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send({hi : 'there'});
-});
+require('./services/passport');
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
