c69de62a32dd3a26589636e53aa34a26e3c90969
Piyush <psantwani@gmail.com>
2017-09-20 12:35:59 +0530

    prod and dev keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
-module.exports = {
-    googleClientID: '769136930839-5efqso4kf3pfc2vsscmhvmfpk40hq347.apps.googleusercontent.com',
-    googleClientSecret: 'SiTQUHynuUYH5lDnTd7irsx4',
-    mongoURI: 'mongodb://psantwani:password@ds141284.mlab.com:41284/react-emaily-dev',
-    cookieKey: 'adsomwem442f3dbfmpom2nfenoinf23pnfw32r23fqe'
+if(process.env.NODE_ENV === 'production'){
+	module.exports = require('./prod');
+}
+else{
+	module.exports = require('./dev');
 }
