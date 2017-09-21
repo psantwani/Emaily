df21f72c73ccb635fba79d33324db1216640cb41
Piyush <psantwani@gmail.com>
2017-09-21 22:40:12 +0530

    code cleaning

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
-    facebookAppID: process.env.FACEBOOK_APP_ID
+    facebookAppID: process.env.FACEBOOK_APP_ID,
     facebookAppSecret: process.env.FACEBOOK_APP_SECRET
 }
 
