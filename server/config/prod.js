291df8b00134b15e87dde6d62bdc88fc16752135
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-09-22 19:00:32 +0530

    stripe publishable and secret key setup. react-stripe-checkout component installed.

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,6 +3,8 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     facebookAppID: process.env.FACEBOOK_APP_ID,
     facebookAppSecret: process.env.FACEBOOK_APP_SECRET
 }
