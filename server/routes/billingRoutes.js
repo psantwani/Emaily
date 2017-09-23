bbc6e69126b23634545950aa04b3ad2fdf3b04aa
Piyush <psantwani@gmail.com>
2017-09-23 13:52:01 +0530

    Stripe create charge API implemented in the backend.Probably will add a middleware to protect APIs from unauthenticated users

---
+++ server/routes/billingRoutes.js
@@ -0,0 +1,23 @@
+const keys = '../config';
+const stripe = require("stripe")(keys.stripeSecretKey
+);
+
+module.exports = app => {
+    app.post('/api/stripe', async (req, res) => {
+        if(!req.user){
+            return res.status(401).send({ error: 'You must be logged in!' });
+        }
+        
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: "usd",
+            source: req.body.id,
+            description: "$5 for 5 credits"
+        });
+
+        req.user.credits += 5;
+        const user = await req.user.save();
+        
+        res.send(user);        
+    });
+};
