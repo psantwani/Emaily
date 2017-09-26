d1473af710255d37b704242e1edb2ec1fbc090da
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-09-26 18:57:38 +0530

    adding new middleware to check minimum credits before creating a survey. Creating a mailer object. Installing sendgrid client

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,6 +5,7 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY,
     facebookAppID: process.env.FACEBOOK_APP_ID,
     facebookAppSecret: process.env.FACEBOOK_APP_SECRET
 }
