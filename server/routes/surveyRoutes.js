0effdc52a6882df4954bdfa4f207fbd8fc4ff814
Piyush <psantwani@gmail.com>
2017-09-30 01:42:18 +0530

    survey list API

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -8,7 +11,15 @@ const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey
+        .find({_user: req.user.id})
+        .select({recipients: false});
+
+        res.send(surveys);
+    });
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
@@ -37,4 +48,36 @@ module.exports = (app) => {
         }
         
     });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if(match){
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact() //remove undefined elements in array
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1},
+                    $set: { 
+                        'recipients.$.responded': true
+                    },
+                    lastResponded: new Date()
+                });
+            })
+            .value();
+        
+        res.send({});
+    });
+    
 }
