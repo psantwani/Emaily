819c107819697656ea8ae802761ff9b060104afc
Piyush <psantwani@gmail.com>
2017-09-29 18:05:59 +0530

    redux-form react-router implemented. Wizard type of form used. Redirection learnt

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,15 +8,13 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-//Development only - axios helpers.
+// Development only axios helpers!
 import axios from 'axios';
 window.axios = axios;
 
-
-
-const store = createStore( reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-	<Provider store={store}><App /></Provider>, 
-	document.querySelector('#root')
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
 );
